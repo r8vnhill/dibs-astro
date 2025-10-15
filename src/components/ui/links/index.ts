f2cbb1b95697910fe0aa9e56c61c3583d071cc4b
r8vnhill <reachme@ravenhill.cl>
2025-10-15 20:38:42 -0300

    Add Python scripting notes and comparison with PowerShell
    - Introduced a new page for Python scripting, highlighting its strengths and limitations compared to PowerShell.
    - Updated existing notes to include a new PythonLink component.
    - Modified PowerShell example to clarify variable naming.
    - Enhanced the overall structure and content for better clarity and reference.

--- src/components/ui/links/index.ts
+++ src/components/ui/links/index.ts
@@ -1,4 +1,5 @@
-export { default as Link } from "./Link.astro";
-export { default as LangLink } from "./LangLink.astro";
-export { default as DibsSourceLink } from "./DibsSourceLink.astro";
 export { default as BashLink } from "./BashLink.astro";
+export { default as DibsSourceLink } from "./DibsSourceLink.astro";
+export { default as LangLink } from "./LangLink.astro";
+export { default as Link } from "./Link.astro";
+export { default as PythonLink } from "./PythonLink.astro";
---
+++ src/layouts/index.ts
@@ -0,0 +1,3 @@
+export { default as LinksLayout } from "./LinksLayout.astro";
+export { default as NotesLayout } from "./NotesLayout.astro";
+export { default as NotesSection } from "./NotesSection.astro";
